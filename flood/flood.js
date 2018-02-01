1cebaa2bb7942dfbe0fb475249157adbf7b62c47
Elsie Hsieh <8chingen8@gmail.com>
2018-02-01 16:42:09 +0800

    flood加入判斷選單出現與否

--- flood/flood.js
+++ flood/flood.js
@@ -7,5 +7,10 @@
 $(document).ready(function () {
     $("#selectDisplay").change(function () {
         $("img").attr("src", "img/" + $("#selectDisplay").val() + ".mp4");
+        if ($("#selectDisplay").val() != "flood") {
+            $(".buttonMenu").css("display", "block");
+        } else {
+            $(".buttonMenu").css("display", "none");
+        }
     });
 });
