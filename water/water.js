2a50522cfa9c7e045f9b9c0d4d193d347075af85
Elsie Hsieh <8chingen8@gmail.com>
2018-03-15 17:38:10 +0800

    修改水資源

--- water/water.js
+++ water/water.js
@@ -1,48 +1,31 @@
-// $(function () {
-//     $('#colorselector').change(function () {
-//         $('.colors').hide();
-//         $('#' + $(this).val()).show();
+// $(document).ready(function() {
+//     $("#selectDisplay").change(function() {
+//         $("img.show").attr("src", "img/" + $("#selectDisplay").val() + ".png");
 //     });
 // });
-$(document).ready(function () {
-    $("#selectDisplay").change(function () {
-        $("img.show").attr("src", "img/" + $("#selectDisplay").val() + ".png");
+
+//#selectScenario選擇none，剩下三個select都disabled，呈現首頁圖
+//row1 img 左7右5 row2說明
+//#selectScenario選擇statistical_downscaling，移除select disabled
+//.show.left img/base.png
+//隱藏row2說明
+//隱藏.show.right圖片
+//顯示.
+$(document).ready(function() {
+    $("#selectScenario").change(function() {
+        if ($(this).val() == "statistical_downscaling") {
+            $('#selectYear').attr('disabled', false);
+            $('#selectDisplay').attr('disabled', false);
+            $('#selectLocation').attr('disabled', false);
+            // $("img.show").attr("src", "img/" + $("#selectDisplay").val() + ".png");
+            $(".statistical.container").css("display", "block");
+            $(".default.container").css("display", "none");
+        } else {
+            $('#selectYear').attr('disabled', true);
+            $('#selectDisplay').attr('disabled', true);
+            $('#selectLocation').attr('disabled', true);
+            $(".statistical.container").css("display", "none");
+            $(".default.container").css("display", "block");
+        }
     });
 });
-
-//  $(document).ready(function () {
-//      $(document).on("scroll", onScroll);
-//      //smoothscroll
-//      $('.navbar-nav li a[href^="#"]').on('click', function (e) {
-//          e.preventDefault();
-//          $(document).off("scroll");
-//          $('.navbar-nav li a').each(function () {
-//              $(this).removeClass('active');
-//          })
-//          $(this).addClass('active');
-//          var target = this.hash,
-//              menu = target;
-//          target = $(target);
-//          $('html, body').stop().animate({
-//              'scrollTop': target.offset().top + 2
-//          }, 1000, 'swing', function () {
-//              window.location.hash = target;
-//              $(document).on("scroll", onScroll);
-//          });
-//      });
-//  });
-
-// function onScroll(event) {
-//     var scrollPos = $(document).scrollTop();
-//     $('.navbar-nav li a').each(function () {
-//         var currLink = $(this);
-//         var refElement = $(currLink.attr("href"));
-//         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-//             $('.navbar-nav li a').removeClass("active");
-//             currLink.addClass("active");
-//         }
-//         else {
-//             currLink.removeClass("active");
-//         }
-//     });
-// }
