e3ad9aee639281db8fca3c67cde4a7521fc28bb7
Elsie Hsieh <8chingen8@gmail.com>
2018-03-19 18:10:42 +0800

    water near_future location

--- water/water.js
+++ water/water.js
@@ -1,16 +1,3 @@
-// $(document).ready(function() {
-//     $("#selectDisplay").change(function() {
-//         $("img.show").attr("src", "img/" + $("#selectDisplay").val() + ".png");
-//     });
-// });
-
-//#selectScenario選擇none，剩下三個select都disabled，呈現首頁圖
-//row1 img 左7右5 row2說明
-//#selectScenario選擇statistical_downscaling，移除select disabled
-//.show.left img/base.png
-//隱藏row2說明
-//隱藏.show.right圖片
-//顯示.
 $(document).ready(function() {
     $("#selectScenario").change(function() {
         if ($(this).val() == "statistical_downscaling") {
@@ -28,4 +15,47 @@ $(document).ready(function() {
             $(".default.container").css("display", "block");
         }
     });
+
+
+
+    $("#selectYear").change(function() {
+        if ($(this).val() == "near_future") {
+            $('#selectDisplay').attr('disabled', false);
+            $('#selectLocation').attr('disabled', false);
+
+            $('select[id*="selectLocation"] option[value="public_taipei-banshin-taoyuan"]').hide();
+            $('select[id*="selectLocation"] option[value*="ag"]').hide();
+            $('select[id*="selectLocation"] option[value="public_taipei"]').attr('selected', true);
+            $(".statistical.container .show.left").attr("src", "img/near_future_" + $('#selectLocation').val() + ".png");
+            text_var = text.text_ + $('#selectLocation').val();
+            $('.statistical.container .card-text').text(text_var);
+            $("#selectLocation").change(function() {
+                $(".statistical.container .show.left").attr("src", "img/near_future_" + $('#selectLocation').val() + ".png");
+                if ($("#selectLocation").val() == "public_banshin") {
+                    $('.statistical.container .card-text').text(text.text_public_banshin);
+                } else if ($("#selectLocation").val() == "public_taoyuan") {
+                    $('.statistical.container .card-text').text(text.text_public_taoyuan);
+                } else {
+                    $('.statistical.container .card-text').text(text.text_public_taipei);
+                }
+            });
+        } else {
+            $('select[id*="selectLocation"] option[value="public_taipei-banshin-taoyuan"]').show();
+            $('select[id*="selectLocation"] option[value*="ag"]').show();
+            $('#selectDisplay').attr('disabled', true);
+            $('#selectLocation').attr('disabled', true);
+            $(".statistical.container .show.left").attr("src", "img/base_left.png");
+            $('.statistical.container .card-text').text(text.text_base);
+
+
+
+        }
+    });
 });
+
+var text = {
+    text_public_taipei: "臺北地區近未來氣候變遷不影響供水。",
+    text_public_banshin: "板新地區主要缺水發生在1月至3月期間，近未來在板新二期計畫啟動之後，缺水率明顯降低，近未來五個GCM結果皆顯示氣候變遷不影響供水。",
+    text_public_taoyuan: "桃園地區主要缺水發生在1月至3月期間。",
+    text_base: "現況臺北缺水率為0，板新與桃園地區在枯水期有些微缺水，但均在容許範圍。臺北SI缺水指數設計標準為0 .1， 板新與桃園設計標準為0 .5， 結果顯示均在設計範圍內。"
+};
