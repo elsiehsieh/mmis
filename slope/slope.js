6b239b624cbb7081b59f097d36a7e13678ed92c1
Elsie Hsieh <8chingen8@gmail.com>
2018-04-24 17:06:32 +0800

    add comments

--- slope/slope.js
+++ slope/slope.js
@@ -1,12 +1,13 @@
-$(document).ready(function () {
-    $("#eventSelect").change(function () {
+$(document).ready(function() {
+    $("#eventSelect").change(function() {
         $("img.show").attr("src", "img/" + $("#eventSelect").val() + ".png");
     });
 });
-$(document).ready(function () {
-    $("#displayTypeSelect").change(function () {
+$(document).ready(function() {
+    $("#displayTypeSelect").change(function() {
         $("img.show").attr("src", "img/" + $("#displayTypeSelect").val() + ".png");
         if ($(this).val() == "facility") {
+            //如果選設施要出現第三層選單
             $(".facilitySelectDiv").css("display", "inline");
             $(".waterSelectDiv").css("display", "none");
             $("img.show").attr("src", "img/" + $("#facilitySelect").val() + ".png");
@@ -20,13 +21,14 @@ $(document).ready(function () {
         }
     });
 });
-$(document).ready(function () {
-    $("#facilitySelect").change(function () {
+$(document).ready(function() {
+    //依據選取的option更改讀取的圖片路徑
+    $("#facilitySelect").change(function() {
         $("img.show").attr("src", "img/" + $("#facilitySelect").val() + ".png");
     });
 });
-$(document).ready(function () {
-    $("#waterSelect").change(function () {
+$(document).ready(function() {
+    $("#waterSelect").change(function() {
         $("img.show").attr("src", "img/" + $("#waterSelect").val() + ".png");
     });
 });
--- water/water.js
+++ water/water.js
@@ -6,18 +6,20 @@ $(document).ready(function() {
     var selectDisplayOptions = "";
     var selectLocationOptions = "";
 
+    //選氣候情境
     $("#selectScenario").on("change", function() {
-        console.log("選擇情境");
         var value = $(this).val();
+        //選統計降尺度
         if (value == "statistical_downscaling") {
+            //出現第二層「用水分析目標年度」選單
             selectYearOptions =
                 "<option>請選擇</option>" +
                 "<option value='base'>基期(1986~2005)</option>" +
                 "<option value='near_future'>近未來(2016~2035)</option>";
             $("#selectYear").html(selectYearOptions);
+            //hide預設頁面
             $(".statistical.container").css("display", "block");
             $(".default.container").css("display", "none");
-
         } else {
             $(".statistical.container").css("display", "none");
             $(".default.container").css("display", "block");
@@ -33,22 +35,33 @@ $(document).ready(function() {
                 .remove();
         }
     });
-
+    // 選年度
     $("#selectYear").on("change", function() {
         var value = $(this).val();
         if (value == "base") {
+            //基期
+            //呈現主題成果與區域
             selectDisplayOptions =
                 "<option selected='selected' value='public_water'>公共用水</option>";
             $("#selectDisplay").html(selectDisplayOptions);
             selectLocationOptions =
                 "<option selected='selected' value='public_taipei-banshin-taoyuan'>臺北、板新、桃園</option>";
             $("#selectLocation").html(selectLocationOptions);
+            //換圖片與文字
             $(".statistical.container .show.left").attr("src", "img/base_left.png");
             $('.statistical.container .card-text').text(text.text_base);
+
+            $(".card-right").show();
+            $(".card-bottom").hide();
+            $(".img-show-9").show();
+            $(".img-show-12").hide();
         } else if (value == "near_future") {
+            //近未來
+            //清除「區域」選單
             $("#selectLocation")
                 .find("option")
                 .remove();
+            //更新「主題成果」選項
             selectDisplayOptions =
                 "<option>選擇主題成果</option>" +
                 "<option value='public_water'>公共用水</option>" +
@@ -63,10 +76,11 @@ $(document).ready(function() {
                 .remove();
         }
     });
-
+    //選主題成果
     $("#selectDisplay").on("change", function() {
         var value = $(this).val();
         if (value == "public_water") {
+            //公共用水
             selectLocationOptions =
                 "<option>選擇區域</option>" +
                 "<option value='public_taipei'>臺北</option>" +
@@ -74,12 +88,14 @@ $(document).ready(function() {
                 "<option value='public_taoyuan'>桃園</option>";
             $("#selectLocation").html(selectLocationOptions);
         } else if (value == "ag_water") {
+            //農業用水
             selectLocationOptions =
                 "<option value='ag_none'>選擇區域</option>" +
                 "<option value='ag_shimen'>石門灌區</option>" +
                 "<option value='ag_taoyuan'>桃園灌區</option>";
             $("#selectLocation").html(selectLocationOptions);
         } else {
+            //預設清除不選
             $("#selectLocation")
                 .find("option")
                 .remove();
@@ -88,13 +104,16 @@ $(document).ready(function() {
 
     $("#selectLocation").on("change", function() {
         var value = $(this).val();
-
+        //如果是選公共用水下的區域
         if (value == "public_taipei" || value == "public_banshin" || value == "public_taoyuan") {
             $(".statistical.container .show.left").attr("src", "img/" + value + ".png");
             $('.statistical.container .card-text').text(text['text_' + value]);
         }
-
+        //如果選農業用水下的區域
         if (value == "ag_shimen" || value == "ag_taoyuan") {
+            //hide左右配置版型
+            //show上下配置版型
+            //原因：農業用水的文字都很長，如果維持左右版型，文字框會拉很長
             $(".card-right").hide();
             $(".card-bottom").show();
             $(".img-show-9").hide();
@@ -102,11 +121,13 @@ $(document).ready(function() {
             $(".statistical.container .show.left").attr("src", "img/" + value + ".png");
             $('.statistical.container .card-text').text(text['text_' + value]);
         } else if (value == "ag_none") {
+            //「區域」選單「選擇區域」選項
             $(".card-right").hide();
             $(".card-bottom").show();
             $(".img-show-9").hide();
             $(".img-show-12").show();
         } else {
+            //預設顯示左右版型
             $(".card-right").show();
             $(".card-bottom").hide();
             $(".img-show-9").show();
